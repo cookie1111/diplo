Fixed everything should be operational now. Had to add a squeeze since the dataLoader doesn't get rid of the empty dim, removed the transfer function use in the coefficient calculation. Now all the splitting is doen outside since the IMF is ran on the whole available history for the given iteration

MEEMD-GMDH.py
@@ -10,12 +10,12 @@ from math import floor
 from typing import Callable
 
 
-TEST = 10
+TEST = 11
 
 
 class MEEMDGMDH:
 
-    def __init__(self, ts: np.ndarray, ratio) -> None:
+    def __init__(self, ts: np.ndarray) -> None:
         self.timeseries = ts
         self.models = None
         self.model_res = None
@@ -23,14 +23,14 @@ class MEEMDGMDH:
     def add_noise(self, noise_amp):
         return self.timeseries + np.random.normal(0, noise_amp, len(self.timeseries))
 
-    def get_imfs(self, timeseries, upper_limit, amount_of_imfs: int = 9):
+    def get_imfs(self, timeseries, upper_limit, cut_off = 0, amount_of_imfs: int = 9):
         s = sift.sift(timeseries[:upper_limit], max_imfs=amount_of_imfs)
-        imfs = np.array(s)
-        res = self.timeseries[:upper_limit] - np.sum(imfs, axis=-1)
+        imfs = np.array(s[cut_off:, :])
+        res = self.timeseries[cut_off:upper_limit] - np.sum(imfs, axis=-1)
         return imfs, res
 
     def create_ensamble_imfs(self, procs: int = 1, noise_amp: float = 0.05,
-                             use_split: float = 0.75) -> tuple[list[np.ndarray], np.ndarray]:
+                             use_split: float = 0.75, cut_off: int = 0) -> tuple[list[np.ndarray], np.ndarray]:
         """
         Calculate imfs for the given timeseries, and correct set(train/select/val) for each set the same start is
         used, just the cut-off point changes
@@ -38,19 +38,21 @@ class MEEMDGMDH:
         :param procs: amount of processes used CURRENTLY NOT IMPLEMENTED
         :param noise_amp: amplitude of the added gaussian noise
         :param use_split: add the upper limit -> whole set == 1, 0.5 means the upper limit is at hald of the set
+        :param cut_off: starting position for the imfs(for example if its a select set this mean that we don't want to
+        include the training part in the select)
         :return: imfs + res
         """
         if procs > 1:
             pass
         else:
-            upper_limit = np.floor(len(self.timeseries)*use_split)
+            upper_limit = floor(len(self.timeseries)*use_split)
             noise_width = noise_amp * np.abs(np.max(self.timeseries[:upper_limit]) - np.min(self.timeseries[
                                                                                             :upper_limit]))
             number_ensamble_memebers = 1000
             all_imfs = {}
             all_res = []
             for i in range(number_ensamble_memebers):
-                imf, res = self.get_imfs(self.add_noise(noise_width), upper_limit)
+                imf, res = self.get_imfs(self.add_noise(noise_width), upper_limit, cut_off=cut_off)
                 for j in range(imf.shape[1]):
                     if j in all_imfs:
                         all_imfs[j].append(np.insert(imf[:, j], 0, i))
@@ -70,7 +72,7 @@ class MEEMDGMDH:
         res_median = [nup]
         return imfs_medians, res_median
 
-    def gmdh_train(self, train_x, train_y,
+    def gmdh_train(self, train_x, train_y, select_x, select_y,
                    fitness_fn: tuple[tuple[Callable]] | list[tuple[Callable]] =
                    ((utils.poly, lambda x: x),
                     (utils.sigmoid, utils.inverse_sigmoid),
@@ -82,10 +84,11 @@ class MEEMDGMDH:
         model = utils.GMDHSlim(transfer_functions=fitness_fn,
                                error_function=err_function,
                                train_select_split=split)
-        model.construct_GMDH(train_x, train_y, stop_leniency=3)
+        model.construct_GMDH(train_x, train_y, select_x, select_y, stop_leniency=3)
         return model
 
-    def train_sets(self, splits: tuple[float, float, float] = 0.75,) -> None:
+    # DONE
+    def train_sets(self, fitness_fns, splits: tuple[float, float, float] = (0.75 * 0.8, 0.8, 1),) -> None:
         """
         Calculates the imfs for the ensambles and trains the corresponding models
 
@@ -95,17 +98,36 @@ class MEEMDGMDH:
         self.models_imfs = []
         #train and selection
         imfs_train, res_train = self.create_median(*self.create_ensamble_imfs(use_split=splits[0]))
-        imfs_select, res_select = self.create_median(*self.create_ensamble_imfs(use_split=splits[1]))
-        imfs_test, res_test = self.create_median(*self.create_ensamble_imfs(use_split=splits[2]))
-        #medians_imfs, median_res = self.create_median(imfs, res)
-        for imf in medians_imfs:
+
+        imfs_select, res_select = self.create_median(
+            *self.create_ensamble_imfs(use_split=splits[1], cut_off=floor(len(self.timeseries) * splits[0]))
+        )
+        # cut out only the relevant part
+        imfs_test, res_test = self.create_median(
+            *self.create_ensamble_imfs(use_split=splits[2], cut_off=floor(len(self.timeseries) * splits[1]))
+        )
+        print(f"Train:{imfs_train[0].shape}, Select:{imfs_select[0].shape}, Test:{imfs_test[0].shape}")
+
+        for imf in zip(imfs_train, imfs_select, imfs_test):
             print("place_holder")
-            dloader = DataLoader(imf)
-            train_split, val_split = dloader.window_split_train_val_x_y(window_size=7)
-            self.models[imf] = self.gmdh_train(*train_split, err_function = mean_square_error, split = split)
-        dloader = DataLoader(res)
-        train_split, val_split = dloader.window_split_train_val_x_y(window_size=7)
-        self.model_res = self.gmdh_test(*train_split, mean_square_error, split)
+            dloader_train = DataLoader(imf[0])
+            dloader_select = DataLoader(imf[1])
+            dloader_test = DataLoader(imf[2])
+
+            train_split = dloader_train.window_split_x_y(window_size=7)
+            select_split = dloader_select.window_split_x_y(window_size=7)
+
+            print(f"{train_split[0].shape} , {train_split[1].shape}")
+            print(f"{select_split[0].shape} , {select_split[1].shape}")
+            self.models[imf] = self.gmdh_train(*train_split, *select_split,
+                                               fitness_fn=fitness_fns,
+                                               err_function=mean_square_error)
+        dloader_train = DataLoader(res_train)
+        dloader_select = DataLoader(res_select)
+        dloader_test = DataLoader(res_test)
+        train_split = dloader_train.window_split_x_y(window_size=7)
+        select_split = dloader_select.window_split_x_y(window_size=7)
+        self.model_res = self.gmdh_train(*train_split, *select_split, mean_square_error)
 
     def test(self, inputs=None):
         if inputs is None:
@@ -212,3 +234,14 @@ if __name__ == '__main__':
                                ], )
         # (utils.radial_basis, utils.inverse_radial_basis)],)
         gmdh.construct_GMDH(matrix[:, :-1], matrix[:, -1], 3)
+
+    if TEST == 11:
+        s = utils.normalize_ts(s, 0.8)
+        meme = MEEMDGMDH(s)
+        meme.train_sets([(utils.poly, lambda x: x),
+                         (utils.sigmoid, utils.inverse_sigmoid),
+                         (utils.hyperbolic_tangent, utils.inverse_hyperbolic_tangent),
+                         (utils.radial_basis, utils.inverse_radial_basis)
+                         ],
+                        )
+

utils.py
@@ -5,7 +5,7 @@ from typing import Callable, Union
 from math import floor
 from random import sample
 import warnings
-warnings.filterwarnings("error")
+#warnings.filterwarnings("error")
 
 # use scipy for value estimation
 from scipy.optimize import least_squares
@@ -344,7 +344,6 @@ Neuron specification:
         """
         return c[0], np.array([c[1], c[2]]), np.array([[c[3], c[5] / 2], [c[5] / 2, c[4]]])
 
-    # TODO add a check for correct shape for multiplication
     def calc_quadratic_matrix(self, x1: np.ndarray, x2: np.ndarray | None = None) -> np.ndarray:
         """
         calculate this neurons output based on previous layers input
@@ -578,7 +577,9 @@ def poly(coeff: np.ndarray | list, x: np.ndarray, use_poly=True):
     dos = (uno.T * x.T).sum(-1)
     tres = np.dot(x.T, coeffs[1])
     # # print(tres.shape)
-    return coeffs[0] + tres + dos
+    res = coeffs[0] + tres + dos
+    #print(f"Last line of defence: {x.shape}, {res.shape}")
+    return res
 
 
 def radial_basis(coeffs, x, use_poly = True):
@@ -627,8 +628,8 @@ def normalize_ts(ts, ratio):
 
 class GMDHSlim:
 
-    def __init__(self, transfer_functions: list[tuple[Callable, Callable]] = [],
-                 error_function: Callable= mean_square_error,
+    def __init__(self, transfer_functions: list[tuple[Callable, Callable]] | tuple[tuple[Callable, Callable]] = [],
+                 error_function: Callable = mean_square_error,
                  train_select_split: float = 0.75, max_layer_size: int = 128):
         self.transfer_functions = transfer_functions
         self.error_function = error_function
@@ -643,13 +644,13 @@ class GMDHSlim:
         i = 1
         print("Starting training of neural network")
         while 1:
-            print(f"Training layer {i}")
+            print(f"Training layer {i}, current shapes:{X_train.shape}, {X_select.shape}")
             self.layers.append(MatrixGMDHLayer(self.transfer_functions, self.error_function, self.ts_split,
                                                self.max_layer_size))
             cur_best_neur = self.layers[-1].train_layer(X_train, y_train, X_select, y_select)
             X_train = self.layers[-1].forward(X_train)
             X_select = self.layers[-1].forward(X_select)
-            #print(X)
+            print(f"In construct GMDH after one pass{X_train.shape}, {X_select.shape}")
             print(f"Layer {i} trained")
 
             if cost > cur_best_neur[0]:
@@ -695,8 +696,9 @@ class MatrixGMDHLayer:
         :param transfer_func: transfer function that accepts coeff, X
         :return: coeff with the highest value
         """
-
-        res = least_squares(lambda coeffs, ts_x, ts_y, ts_f: ts_f(None, ts_y, use_poly=False) - ts_f(coeffs, ts_x),
+        # removed the transformation on ts_y
+        #print(f"We going into poly territory: {X.shape}, {y.shape}")
+        res = least_squares(lambda coeffs, ts_x, ts_y, ts_f: np.squeeze(ts_y) - poly(coeffs, ts_x),
                             np.array([3, 1, 2, 1, 1, 1]), args=(X, y, transfer_func))
         return res.x, res.cost
 
@@ -755,18 +757,25 @@ class MatrixGMDHLayer:
         :return: None
         """
         combs = []
+        cntr = 0
+        all = (X_train.shape[1]*(X_train.shape[1]-1))/2
         X_train = transfer_fn[0](None, X_train, False)
         X_select = transfer_fn[0](None, X_select, False)
+        #print(f"Train combinations({transfer_fn[0].__name__}): {X_train.shape}, {X_select.shape}")
         #y = transfer_fn[0](None, y, False)
         for i in comb(range(X_train.shape[1]), 2):
-
+            if cntr % 500 == 0:
+                print(f"{cntr} out of {all} combinations trained")
             train_matrix_x = np.array([X_train[:, i[0]], X_train[:, i[1]]])
             test_matrix_x = np.array([X_select[:, i[0]], X_select[:, i[1]]])
+
+            #print(f"Combinations: {train_matrix_x.shape}, {test_matrix_x.shape}")
             res = self.calc_poly_coeff(train_matrix_x, y_train, poly)
             coeffs = res[0]
 
             mse_poly = self.evaluate_poly(coeffs, test_matrix_x, y_select, transfer_fn, cost_fn)
             combs.append((mse_poly, i, coeffs, transfer_fn))
+            cntr = cntr + 1
         return combs
 
     # DONE
@@ -807,6 +816,7 @@ class MatrixGMDHLayer:
             costs_prev.append((cost_function(X_select[:, i], y_select), (i, -1), "TRANSFER"))
         costs_prev.sort()
         for tf in transfer_functions:
+            #print(f"Layer train: {X_train.shape}, {X_select.shape}")
             costs_prev = costs_prev + self.train_combinations_tf(X_train, y_train, X_select, y_select, tf,
                                                                  cost_function)
 